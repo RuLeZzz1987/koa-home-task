098ea2cb3348cd32de802251fdcdd7e710c3a4c2
Alexander Duka <a.duka@aimprosoft.com>
2017-04-05 15:30:30 +0300

    user remove updated

--- src/services/user/remove.js
+++ src/services/user/remove.js
@@ -1,7 +1,19 @@
+import Sequelize from "sequelize";
 import models from "../../models";
 
-export default async (ctx, next) => {
+const { ForeignKeyConstraintError } = Sequelize;
+
+export default async ctx => {
   const { user } = ctx.state;
-  const result = await models.User.destroy({where: {id: user.id}});
-  ctx.body={status: "success", message: "Your account has been removed"};
+  try {
+    await models.User.destroy({ where: { id: user.id } });
+    ctx.body = { status: "success", message: "Your account has been removed" };
+  } catch (e) {
+    if (e instanceof ForeignKeyConstraintError) {
+      ctx.status = 403;
+      ctx.body = {status: "error", message: "Can not remove user which is the owner of the company"}
+    } else {
+      throw e;
+    }
+  }
 }
