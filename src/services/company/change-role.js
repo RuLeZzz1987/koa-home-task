ab88cfb0cf8389acf007feb0c8a1f1716de7c607
Alexander Duka <a.duka@aimprosoft.com>
2017-04-06 15:58:13 +0300

    change & remove user permissions

--- src/services/company/change-role.js
+++ src/services/company/change-role.js
@@ -1,3 +1,27 @@
+import _ from "lodash";
+
 export default async (ctx, id, userId, next) => {
-  ctx.body = {action: `change user ${userId} role into company ${id}`};
+  const role = _.get(ctx.request, "body.role", "user");
+  const { company, transaction } = ctx;
+  const { user } = ctx;
+
+  try {
+    /*
+     * search for "// Or by providing a second options.through argument when"
+     * http://docs.sequelizejs.com/en/latest/docs/associations/#associating-objects
+     * */
+    user.Role = {
+      role
+    };
+
+    await company.setEmployee(user, { transaction });
+    await transaction.commit();
+    ctx.body = {
+      status: "success",
+      message: `Permissions ${role} were granted to user with name ${user.login}`
+    };
+  } catch (e) {
+    transaction.rollback();
+    throw e;
+  }
 }
--- src/services/company/remove-user.js
+++ src/services/company/remove-user.js
@@ -1,3 +1,20 @@
+import _ from "lodash";
+
 export default async (ctx, id, userId, next) => {
-  ctx.body = {action: `remove user ${userId} to ${id} company`};
+  const role = _.get(ctx.request, "body.role", "user");
+  const { company, transaction } = ctx;
+  const { user } = ctx;
+
+  try {
+
+    await company.removeEmployee(user, { transaction });
+    await transaction.commit();
+    ctx.body = {
+      status: "success",
+      message: `User with name ${user.login} was removed from the company ${company.name}`
+    };
+  } catch (e) {
+    transaction.rollback();
+    throw e;
+  }
 }
